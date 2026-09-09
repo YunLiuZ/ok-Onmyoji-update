6f941306c868cdd62ddb13d6d65be870dddeed54
ok-oldking <ok-oldking@users.noreply.github.com>
2026-09-09 06:43:46 +0000

    周年庆爬塔

--- src/config.py
+++ src/config.py
@@ -4,7 +4,7 @@ import sys
 import numpy as np
 from ok import ConfigOption
 
-version = "v1.3.1"
+version = "v1.3.2"
 #不需要修改version, Github Action打包会自动修改
 
 
--- src/tasks/GameEventsBattleTask.py
+++ src/tasks/GameEventsBattleTask.py
@@ -9,7 +9,7 @@ class GameEventsBattleTask(BaseBattleTask):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.name = "活动-战斗-当期爬塔"
-        self.description = "2026.8.19洛天依联动爬塔"
+        self.description = "2026.9.9周年庆爬塔,建议每日手动领取活动票，消除弹窗"
         self.count = 0
 
 
@@ -45,44 +45,66 @@ class GameEventsBattleTask(BaseBattleTask):
         self.Battle()
 
     def Battle_page(self):
-        self.click_rect_random((0.33, 0.34, 0.4, 0.45))
-        self.sleep(1)
-        if self.wait_click_ocr(match=re.compile("神威|破障|战斗"),
+        self.click_rect_random((0.35, 0.33, 0.38, 0.37))
+        self.sleep(3)
+        if self.wait_ocr(match=re.compile("战斗|回响|日地"),
                              time_out=3,
-                             box=self.box_of_screen(0.46, 0.5, 0.54, 0.84),
+                             box=self.box_of_screen(0.2, 0.15, 0.37, 0.48),
                              raise_if_not_found=False):
-            self.log_info("进入战斗")
-            return True
-        elif self.wait_ocr(match=re.compile("获得|奖励"),
-                           time_out=3,
-                           box=self.box_of_screen(0.37, 0.24, 0.63, 0.39),
-                           raise_if_not_found=False):
-            self.sleep(0.5)
-            self.click_rect_random((0.86, 0.25, 0.89, 0.31))
+            self.log_info("进入战斗页面")
             self.sleep(1)
-            self.click_rect_random((0.86, 0.25, 0.89, 0.31))
-            if self.wait_click_ocr(match=re.compile("神威|破障|战斗"),
-                                   time_out=3,
-                                   box=self.box_of_screen(0.46, 0.5, 0.54, 0.84),
-                                   raise_if_not_found=False):
+            self.click_rect_random((0.25, 0.27, 0.29, 0.37))
+            self.sleep(3)
+            if self.wait_click_ocr(match=re.compile("虚无|精锐|战斗"),
+                                 time_out=3,
+                                 box=self.box_of_screen(0.01, 0.12, 0.23, 0.24),
+                                 raise_if_not_found=False):
                 self.log_info("进入战斗")
+                self.sleep(1)
                 return True
+        elif self.wait_feature('Event_Finish_2', threshold=0.7,
+                                    box=self.box_of_screen(0.34, 0.22, 0.65, 0.44),
+                                        raise_if_not_found=False,
+                                        time_out=3):
+            self.sleep(0.5)
+            self.click_rect_random((0.89, 0.24, 0.92, 0.29))
+            self.sleep(1)
+            self.click_rect_random((0.7, 0.86, 0.73, 0.93))
+            self.sleep(1)
+            self.click_rect_random((0.89, 0.24, 0.92, 0.29))
+            self.sleep(1)
+            self.click_rect_random((0.89, 0.24, 0.92, 0.29))
+            if self.wait_ocr(match=re.compile("战斗|回响|日地"),
+                             time_out=3,
+                             box=self.box_of_screen(0.2, 0.15, 0.37, 0.48),
+                             raise_if_not_found=False):
+                self.log_info("进入战斗页面")
+                self.sleep(1)
+                self.click_rect_random((0.25, 0.27, 0.29, 0.37))
+                self.sleep(3)
+                if self.wait_click_ocr(match=re.compile("虚无|精锐|战斗"),
+                                       time_out=3,
+                                       box=self.box_of_screen(0.01, 0.12, 0.23, 0.24),
+                                       raise_if_not_found=False):
+                    self.log_info("进入战斗")
+                    self.sleep(1)
+                    return True
         else:
             self.log_warning("没有进入战斗页面")
             return False
 
     def Battle(self):
-        if self.wait_ocr(match=re.compile("养成|协战|式神"),
+        if self.wait_ocr(match=re.compile("虚无|精锐"),
                          time_out=3,
-                         box=self.box_of_screen(0.65,0.82,0.84,0.9)):
-            self.sleep(0.5)
-            if self.ocr(match=re.compile("必定|双倍|掉落"),
-                             box=self.box_of_screen(0.01, 0.75, 0.32, 0.88)):
+                         box=self.box_of_screen(0,0,0.3,0.15)):
+            self.sleep(3)
+            if self.ocr(match=re.compile("御魂|自选"),
+                             box=self.box_of_screen(0.0, 0.60, 0.21, 0.70)):
+                self.log_info("现在是普通模式")
+                self.isap = False
+            else:
                 self.log_info("现在是体力模式")
                 self.isap = True
-            else:
-                self.log_info("现在是爬塔模式")
-                self.isap = False
         else:
             self.log_warning("没有进入战斗页面")
             return False
@@ -92,22 +114,22 @@ class GameEventsBattleTask(BaseBattleTask):
             self.log_info(f"体力爬塔票数{self.ap_tickets}")
             self.log_info(f"普通爬塔票数：{self.general_tickets}")
         else:
-            if text := self.ocr(threshold=0.8,box=self.box_of_screen(0.43, 0.03, 0.49, 0.09)):
+            if text := self.ocr(threshold=0.8,box=self.box_of_screen(0.58, 0.0, 0.66, 0.1)):
                 nums = re.findall(r'\d+', text[0].name)
                 self.general_tickets = int(nums[0]) if nums else 0
                 self.log_info(f"普通爬塔票数：{self.general_tickets}")
 
-            if text := self.ocr(threshold=0.8,box=self.box_of_screen(0.75, 0.03, 0.8, 0.09)):
+            if text := self.ocr(threshold=0.8,box=self.box_of_screen(0.42, 0.0, 0.54, 0.1)):
                 nums = re.findall(r'\d+', text[0].name)
                 self.ap_tickets = int(nums[0]) if nums else 0
                 self.log_info(f"体力爬塔票数{self.ap_tickets}")
 
         if self.config["Lock Team Enable"]:
             # 解锁状态 准备换队伍
-            self.Lock_team((0.61, 0.89, 0.66, 0.97), lock=False)
+            self.Lock_team((0.66, 0.88, 0.72, 0.99), lock=False)
         else:
             # 不换
-            self.Lock_team((0.61, 0.89, 0.66, 0.97), lock=True)
+            self.Lock_team((0.66, 0.88, 0.72, 0.99), lock=True)
 
 
         if self.config["GeneralClimb"]:
@@ -117,12 +139,12 @@ class GameEventsBattleTask(BaseBattleTask):
                 self.next_sleep_count = random.randrange(*(self.count_range))
                 self.log_info(f"下次休息{self.next_sleep_count}")
             if self.isap:
-                self.click_rect_random((0.97, 0.76, 0.99, 0.79))
-                self.log_info("切换为体力爬塔")
+                self.click_rect_random((0.96, 0.76, 0.98, 0.79))
+                self.log_info("切换为普通爬塔")
                 self.isap = False
                 self.sleep(0.5)
             else:
-                self.log_info("爬塔")
+                self.log_info("普通爬塔")
             while self.count < self.general_tickets:
                 self.Battle_process()
                 self.count += 1
@@ -131,16 +153,16 @@ class GameEventsBattleTask(BaseBattleTask):
                     if self.count >= self.next_sleep_count:
                         self.next_sleep_count = self.count + random.randrange(*(self.count_range))
                         a = random.randrange(*(self.time_range))
-                        self.log_info(f"第 {self.count} 次爬塔战斗结束,休息{a}秒，下次休息{self.next_sleep_count}")
+                        self.log_info(f"第 {self.count} 次普通爬塔战斗结束,休息{a}秒，下次休息{self.next_sleep_count}")
                         self.sleep(a)
-                self.log_info(f"第 {self.count} 次爬塔战斗结束 总共{self.general_tickets}，下次休息{self.next_sleep_count}")
+                self.log_info(f"第 {self.count} 次普通爬塔战斗结束 总共{self.general_tickets}，下次休息{self.next_sleep_count}")
 
         if self.config["ApMode"]:
             self.count=0
             if self.is_sleep:
                 self.next_sleep_count = random.randrange(*(self.count_range))
             if not self.isap:
-                self.click_rect_random((0.97, 0.76, 0.99, 0.79))
+                self.click_rect_random((0.96, 0.76, 0.98, 0.79))
                 self.log_info("切换为刷体力")
                 self.isap = True
                 self.sleep(0.5)
@@ -162,43 +184,50 @@ class GameEventsBattleTask(BaseBattleTask):
 
     def Battle_process(self):
         def check():
-            if res := self.wait_feature('Battle_Finish', threshold=0.7,
-                                    box=self.B('Battle_Finish'),
+            if res := self.wait_feature('Event_Finish', threshold=0.7,
+                                    box=self.box_of_screen(0.28, 0.15, 0.71, 0.63),
                                         raise_if_not_found=False,
                                         time_out=1):
                 self.sleep(2)
                 if random.randint(1, 2) == 1:
-                    self.click_rect_random((0.18, 0.77, 0.88, 0.95))
+                    self.click_rect_random((0.3, 0.03, 0.69, 0.28))
                     self.log_info("点击上面")
                 else:
-                    self.click_rect_random(res)
+                    self.click_rect_random((0.3, 0.03, 0.69, 0.28))
                     self.log_info("点击下面")
                 self.sleep(2)
-                if res1 := self.find_one('Battle_Finish', threshold=0.7,
-                                    box=self.B('Battle_Finish')):
+                if res1 := self.find_one('Event_Finish', threshold=0.7,
+                                    box=self.box_of_screen(0.28, 0.15, 0.71, 0.63)):
                     self.sleep(2)
                     if random.randint(1, 2) == 1:
-                        self.click_rect_random((0.18, 0.05, 0.9, 0.21))
+                        self.click_rect_random((0.3, 0.03, 0.69, 0.28))
+                        self.log_info("点击上面")
                     else:
-                        self.click_rect_random(res1)
+                        self.click_rect_random((0.3, 0.03, 0.69, 0.28))
+                        self.log_info("点击下面")
                     self.sleep(0.5)
                     self.log_info("第一次没点到")
                     return True
                 else:
                     self.log_info("第一次点到")
                     return True
-            if res := self.find_one('Battle_Finish_Soul', threshold=0.7,
-                                        box=self.B('Battle_Finish_Soul')):
+            if res := self.find_one('Event_Finish_2', threshold=0.7,
+                                        box=self.box_of_screen(0.28, 0.15, 0.71, 0.63)):
                 if random.randint(1, 2) == 1:
-                    self.click_rect_random((0.18, 0.77, 0.88, 0.95))
+                    self.click_rect_random((0.3, 0.03, 0.69, 0.28))
                     self.log_info("点击上面")
                 else:
-                    self.click_rect_random(res)
+                    self.click_rect_random((0.3, 0.03, 0.69, 0.28))
                     self.log_info("点击下面")
-                self.sleep(1)
-                if res1 := self.find_one('Battle_Finish_Soul', threshold=0.7,
-                                        box=self.B('Battle_Finish_Soul')):
-                    self.click_rect_random((0.18, 0.05, 0.9, 0.21))
+                self.sleep(2)
+                if res1 := self.find_one('Event_Finish_2', threshold=0.7,
+                                         box=self.box_of_screen(0.28, 0.15, 0.71, 0.63)):
+                    if random.randint(1, 2) == 1:
+                        self.click_rect_random((0.3, 0.03, 0.69, 0.28))
+                        self.log_info("点击上面")
+                    else:
+                        self.click_rect_random((0.3, 0.03, 0.69, 0.28))
+                        self.log_info("点击下面")
                     self.log_info("第一次没点到")
                     self.sleep(1)
                     return True
@@ -208,11 +237,11 @@ class GameEventsBattleTask(BaseBattleTask):
 
         if self.config["Lock Team Enable"] and self.count == 2:
             self.log_info("进入第二次战斗锁住阵容")
-            self.Lock_team((0.14, 0.82, 0.2, 0.9), lock=True)
+            self.Lock_team((0.66, 0.88, 0.72, 0.99), lock=True)
 
         if text := self.wait_ocr(match=re.compile("挑战"),
-                                 box=self.box_of_screen(0.88,0.83,0.95,0.91)):
-            self.click_rect_random((0.88, 0.8, 0.96, 0.92))
+                                 box=self.box_of_screen(0.87, 0.8, 0.96, 0.95)):
+            self.click_rect_random((0.88, 0.82, 0.96, 0.94))
         if self.count == 1:
             self.log_info("进入检测1")
             if self.config["Lock Team Enable"]:
